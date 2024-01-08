cd83027d4629ff656af76e1b865837bb5d6b1979
João Guilherme <jaaumgui@gmail.com>
2024-01-08 19:21:43 -0300

    refactor(Pessoa): Adicionado equals e HashCode e anotação toString

--- src/main/java/br/dev/joaoguilherme/basicapi/entity/Pessoa.java
+++ src/main/java/br/dev/joaoguilherme/basicapi/entity/Pessoa.java
@@ -5,17 +5,17 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Getter
 @Setter
 @Entity
+@ToString
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Table(name = "pessoa")
@@ -48,5 +48,20 @@ public class Pessoa {
     @NotBlank(message = "O CPF é obrigatório")
     private String cpf;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return getId() != null && Objects.equals(getId(), pessoa.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 
 }
