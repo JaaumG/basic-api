db5334a5cd64dd623c87512affc35d9d7723cfa4
João Guilherme <jaaumgui@gmail.com>
2024-01-08 12:22:56 -0300

    Projeto iniciado.

---
+++ src/test/java/br/dev/joaoguilherme/basicapi/BasicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.joaoguilherme.basicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
