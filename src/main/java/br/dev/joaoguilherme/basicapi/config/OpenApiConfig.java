8f1bc9f45a836b328d1e74c0241e6295b31d6387
João Guilherme <jaaumgui@gmail.com>
2024-01-08 13:33:01 -0300

    feat(banner): Adicionado banner do spring

---
+++ src/main/java/br/dev/joaoguilherme/basicapi/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package br.dev.joaoguilherme.basicapi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(info = @Info(
+        contact = @Contact(
+                name = "João Guilherme ",
+                email = "mail@joaoguilherne.dev",
+                url = "https://joaoguilherme.dev/"
+        ),
+        title = "Basic API",
+        description = "API básica para estudos",
+        version = "1.0"),
+        servers = {
+                @Server(
+                        description = "localhost",
+                        url = "http://localhost:8080"
+                )
+        }
+)
+public class OpenApiConfig {
+}
