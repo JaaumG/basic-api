601894a1182cc5454fcac92c3f6745ef32ae6d5f
João Guilherme <jaaumgui@gmail.com>
2024-01-08 13:14:02 -0300

    feat(Pessoa): Criado entidade pessoa

---
+++ src/main/java/br/dev/joaoguilherme/basicapi/entity/Pessoa.java
@@ -0,0 +1,52 @@
+package br.dev.joaoguilherme.basicapi.entity;
+
+import br.dev.joaoguilherme.basicapi.enums.Genero;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @NotNull(message = "A data de nascimento é obrigatória")
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "O sexo é obrigatório")
+    @Column(name = "sexo", nullable = false)
+    private Genero sexo;
+
+    @Column(name = "email")
+    @Email(message = "O e-mail é inválido")
+    private String email;
+
+    @CPF
+    @Column(name = "cpf", nullable = false)
+    @NotBlank(message = "O cpf é obrigatório")
+    private String cpf;
+
+
+}
