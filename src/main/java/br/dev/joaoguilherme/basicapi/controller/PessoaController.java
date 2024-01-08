674b0957bf74152f89d6fa17e77b73c927456352
João Guilherme <jaaumgui@gmail.com>
2024-01-08 18:23:16 -0300

    refactor(PessoaService/PessoaController): Aplicado melhores praticas de organização de camadas e status de respostas

--- src/main/java/br/dev/joaoguilherme/basicapi/controller/PessoaController.java
+++ src/main/java/br/dev/joaoguilherme/basicapi/controller/PessoaController.java
@@ -3,6 +3,7 @@ package br.dev.joaoguilherme.basicapi.controller;
 import br.dev.joaoguilherme.basicapi.dto.PessoaCreateDto;
 import br.dev.joaoguilherme.basicapi.dto.PessoaResponseDto;
 import br.dev.joaoguilherme.basicapi.dto.PessoaUpdateDto;
+import br.dev.joaoguilherme.basicapi.entity.Pessoa;
 import br.dev.joaoguilherme.basicapi.services.PessoaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -16,6 +17,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @Validated
@@ -31,35 +37,42 @@ public class PessoaController {
     @Operation(summary = "Busca uma pessoa pelo id")
     public ResponseEntity<PessoaResponseDto> getPessoa(@Parameter(description = "Id da pessoa") @PathVariable Long id){
         log.info("Buscando pessoa com id {}", id);
-        return pessoaService.getPessoa(id);
+        return pessoaService.getPessoa(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.noContent().build());
     }
 
     @GetMapping("/")
     @Operation(summary = "Lista todas as pessoas")
     public ResponseEntity<Page<PessoaResponseDto>> listPessoas(@ParameterObject Pageable pageable){
         log.info("Listando pessoas");
-        return pessoaService.listPessoas(pageable);
+        return ResponseEntity.ok(pessoaService.listPessoas(pageable));
     }
 
     @PostMapping("/")
     @Operation(summary = "Cria uma pessoa")
     public ResponseEntity<String> createPessoa(@RequestBody @Valid PessoaCreateDto pessoaCreateDto){
         log.info("Criando {}", pessoaCreateDto.getNome());
-        return pessoaService.createPessoa(pessoaCreateDto);
+        Pessoa pessoaCriada = pessoaService.createPessoa(pessoaCreateDto);
+        URI location = linkTo(methodOn(PessoaController.class).getPessoa(pessoaCriada.getId())).toUri();
+        return ResponseEntity.created(location).body("Pessoa criada com sucesso");
     }
+
 
     @PutMapping("/{id}")
     @Operation(summary = "Atualiza uma pessoa")
-    public ResponseEntity<String> updatePessoa(@Parameter(description = "Id da pessoa") @PathVariable Long id, @RequestBody @Valid PessoaUpdateDto pessoaUpdateDto){
+    public ResponseEntity<Void> updatePessoa(@Parameter(description = "Id da pessoa") @PathVariable Long id, @RequestBody @Valid PessoaUpdateDto pessoaUpdateDto){
         log.info("Atualizando pessoa com id {}", id);
-        return pessoaService.updatePessoa(id, pessoaUpdateDto);
+        pessoaService.updatePessoa(id, pessoaUpdateDto);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Deleta uma pessoa")
-    public ResponseEntity<String> deletePessoa(@Parameter(description = "Id da pessoa") @PathVariable Long id){
+    public ResponseEntity<Void> deletePessoa(@Parameter(description = "Id da pessoa") @PathVariable Long id){
         log.info("Deletando pessoa com id {}", id);
-        return pessoaService.deletePessoa(id);
+        pessoaService.deletePessoa(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/br/dev/joaoguilherme/basicapi/services/PessoaService.java
+++ src/main/java/br/dev/joaoguilherme/basicapi/services/PessoaService.java
@@ -1,22 +1,20 @@
 package br.dev.joaoguilherme.basicapi.services;
 
-import br.dev.joaoguilherme.basicapi.controller.PessoaController;
 import br.dev.joaoguilherme.basicapi.dto.PessoaCreateDto;
 import br.dev.joaoguilherme.basicapi.dto.PessoaResponseDto;
 import br.dev.joaoguilherme.basicapi.entity.Pessoa;
 import br.dev.joaoguilherme.basicapi.dto.PessoaUpdateDto;
+import br.dev.joaoguilherme.basicapi.exception.NotFoundException;
 import br.dev.joaoguilherme.basicapi.mappings.PessoaMapper;
 import br.dev.joaoguilherme.basicapi.repository.PessoaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.Optional;
 
 @Service
 @Validated
@@ -26,38 +24,32 @@ public class PessoaService {
     private final PessoaRepository repository;
     private final PessoaMapper mapper;
 
-
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<String> createPessoa(PessoaCreateDto pessoaCreateDto){
+    public Pessoa createPessoa(PessoaCreateDto pessoaCreateDto){
         Pessoa pessoa = mapper.toEntity(pessoaCreateDto);
-        pessoa = repository.saveAndFlush(pessoa);
-        return ResponseEntity.created(linkTo(methodOn(PessoaController.class).getPessoa(pessoa.getId())).toUri()).body("Pessoa criada com sucesso");
+        return repository.saveAndFlush(pessoa);
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<PessoaResponseDto> getPessoa(Long id){
-        Pessoa pessoa = repository.findById(id).orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
-        PessoaResponseDto pessoaResponseDto = mapper.toDto(pessoa);
-        return ResponseEntity.ok(pessoaResponseDto);
+    public Optional<PessoaResponseDto> getPessoa(Long id){
+        return repository.findById(id).map(mapper::toDto);
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<String> updatePessoa(Long id, PessoaUpdateDto pessoaUpdateDto){
-        Pessoa pessoa = repository.findById(id).orElseThrow(() -> new RuntimeException("Pessoa não encontrada"));
+    public void updatePessoa(Long id, PessoaUpdateDto pessoaUpdateDto){
+        Pessoa pessoa = repository.findById(id).orElseThrow(() -> new NotFoundException("Pessoa", id));
         pessoa = mapper.partialUpdate(pessoaUpdateDto, pessoa);
         repository.saveAndFlush(pessoa);
-        return ResponseEntity.ok("Pessoa atualizada com sucesso");
     }
 
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<String> deletePessoa(Long id){
+    public void deletePessoa(Long id){
         repository.deleteById(id);
-        return ResponseEntity.ok("Pessoa deletada com sucesso");
     }
 
     @Transactional(readOnly = true)
-    public ResponseEntity<Page<PessoaResponseDto>> listPessoas(Pageable pageable) {
-        Page<PessoaResponseDto> pessoaResponseDtos = repository.findAll(pageable).map(mapper::toDto);
-        return ResponseEntity.ok(pessoaResponseDtos);
+    public Page<PessoaResponseDto> listPessoas(Pageable pageable) {
+        return repository.findAll(pageable).map(mapper::toDto);
     }
+
 }
