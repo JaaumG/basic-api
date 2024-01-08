fb67a7f61ea666c4df732a9c293ecd704d6d25d6
João Guilherme <jaaumgui@gmail.com>
2024-01-08 20:27:50 -0300

    refactor(OpenApiConfig): Melhorado documentação e incluindo SecuritySchema

--- src/main/java/br/dev/joaoguilherme/basicapi/config/OpenApiConfig.java
+++ src/main/java/br/dev/joaoguilherme/basicapi/config/OpenApiConfig.java
@@ -1,13 +1,16 @@
 package br.dev.joaoguilherme.basicapi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(info = @Info(
         contact = @Contact(
-                name = "João Guilherme ",
+                name = "João Guilherme",
                 email = "mail@joaoguilherne.dev",
                 url = "https://joaoguilherme.dev/"
         ),
@@ -16,10 +19,21 @@ import io.swagger.v3.oas.annotations.servers.Server;
         version = "1.0"),
         servers = {
                 @Server(
-                        description = "localhost",
+                        description = "Servidor Local",
                         url = "http://localhost:8080"
                 )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "Basic Authenticate"
+                )
         }
+)
+@SecurityScheme(
+        name = "Basic Authenticate",
+        description = "Autorização do tipo 'Basic' use para usuário 'user' e para senha 'password'.",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
 )
 public class OpenApiConfig {
 }
