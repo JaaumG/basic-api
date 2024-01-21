d179399643ee7706dbd038eae46372225a5b19f8
João Guilherme <jaaumgui@gmail.com>
2024-01-21 19:11:36 -0300

    refactor(Pessoa): Removido entidade pessoa service controller mappers dtos e repository

--- src/main/java/br/dev/joaoguilherme/basicapi/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.dev.joaoguilherme.basicapi.repository;
-
-import br.dev.joaoguilherme.basicapi.entity.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
